3f47554834223b86f5c0ab54e07ad487824b867a
elikjandlt <elikjandlt@gmail.com>
2026-06-23 12:33:45 +0800

    Enlarge header logo

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -31,9 +31,9 @@ export function Header() {
           <Image
             src="/logo.webp"
             alt="RE/MAX UP"
-            width={140}
-            height={48}
-            className="h-10 w-auto"
+            width={180}
+            height={64}
+            className="h-14 w-auto"
             priority
           />
         </Link>
